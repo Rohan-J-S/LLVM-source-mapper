501962b03f77cabe5c331a1981d4da4e1ba1226c
Rohan-J-S <rohan.jayanth.s@gmail.com>
2025-07-23 13:30:56 +0000

    Initial commit

--- test.cpp
+++ test.cpp
@@ -1,35 +1,17 @@
 #include <iostream>
+using namespace std;
 
-// A simple function that adds two integers
-int add(int a, int b) {
-    return a + b;
-}
-
-// A function with some control flow
-int max(int a, int b) {
-    if (a > b) {
-        return a;
-    } else {
-        return b;
-    }
-}
-
-// A function with a loop
-int sum(int n) {
-    int result = 0;
-    for (int i = 1; i <= n; i++) {
-        result += i;
-    }
-    return result;
+// Recursive function to calculate factorial
+int factorial(int n) {
+    if (n <= 1)  // Base case
+        return 1;
+    else
+        return n * factorial(n - 1);  // Recursive call
 }
 
 int main() {
-    int x = 5;
-    int y = 10;
-    
-    std::cout << "Sum: " << add(x, y) << std::endl;
-    std::cout << "Max: " << max(x, y) << std::endl;
-    std::cout << "Sum of 1 to 10: " << sum(10) << std::endl;
-    
+    int number = 5;
+    cout << "Factorial of " << number << " is " << factorial(number) << endl;
     return 0;
 }
+
